2262bc48467f6dafa474f27c238df10858cff765
ssufi63 <67516180+ssufi63@users.noreply.github.com>
2020-07-17 00:50:55 +0600

    wood calculator done

--- assignment.js
+++ assignment.js
@@ -17,3 +17,14 @@ console.log('Exact Miles is: ' + mileResult + ' mile!');
 var mileResult = feetToMile(80000);
     mileResult = mileResult.toFixed(2);
 console.log('Exact Miles is: ' + mileResult + ' mile!');
+
+
+// woodCalculator
+
+function woodCalculator(chair, table, bed){
+    var totalWood = chair * 1 + table * 3 + bed * 5;
+    return totalWood;
+}
+
+var sum = woodCalculator(20, 5, 3);
+console.log('Total wood need: '+ sum);
