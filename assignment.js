f1925dbaae43bb81dfed42cf06584406549a2fe3
ssufi63 <67516180+ssufi63@users.noreply.github.com>
2020-07-17 18:50:56 +0600

    all assigment done

--- assignment.js
+++ assignment.js
@@ -1,22 +1,17 @@
 // feetToMile Converter
 
 // 1 mile = 5280 ft.
-
 function feetToMile(feetValue){
+    var mile = feetValue;
+    if(feetValue >= 0){
         var mile = feetValue / 5280;
         mile = mile.toFixed(2);
-        return mile;
-        
+    }
+    else{
+        console.log('Negative Value not acceptable!');
+    }
+    
 }
-
-
-var mileResult = feetToMile(3000);
-console.log('Exact Miles is: ' + mileResult + ' mile!');
-
-
-var mileResult = feetToMile(80000);
-console.log('Exact Miles is: ' + mileResult + ' mile!');
-
 
 // woodCalculator
 
@@ -25,9 +20,46 @@ function woodCalculator(chair, table, bed){
     return totalWood;
 }
 
-var sum = woodCalculator(20, 5, 3);
-console.log('Total wood need: '+ sum + ' units.');
-
 
 // brickCalculator
 
+
+const uptoTwenty = 150000;
+const aboveTwenty = 270000;
+var totalBricks = 0;
+
+function brickCalculator(storiedBuilding){
+    if(storiedBuilding >=0 && storiedBuilding <=10){
+        totalBricks = storiedBuilding * 15000;
+    }
+
+    else if(storiedBuilding >10 && storiedBuilding <=20){
+        buildingLevel = storiedBuilding - 10;
+        bricks1 = buildingLevel * 12000;
+        totalBricks = uptoTwenty + bricks1;
+    } 
+    else if(storiedBuilding > 20){
+        buildingLevel = storiedBuilding - 20;
+        bricks1 = buildingLevel * 10000;
+        totalBricks = aboveTwenty + bricks1;
+
+    }
+    else{
+        console.log(`can't be a negative storied building!`)
+    }
+    return totalBricks;
+}
+
+
+// tinyFriend
+
+function tinyFriend(names){
+    var smallNames = names[0];
+    for(var i = 0; i < names.length; i++ ){
+        var currentNames = names[i];
+        if (currentNames.length < smallNames.length){
+            smallNames = currentNames;
+        }
+    }
+    return smallNames;
+}
